aa8351b2aed66b27050c81b72e23184cd8ce4e8b
LANDTEAM\janway <janway.kh.gy@gmail.com>
2018-08-02 17:09:33 +0800

    convert Section to ts

--- src/libs/section.ts
+++ src/libs/section.ts
@@ -1,13 +1,15 @@
 /** 一區塊的一行資料 */
-export default class Section {
+export default class Section<T> {
+    drawFunc: String
+    content: Array<String|String[]>
     /** 一區塊的一行資料 */
-    constructor(arr) {
+    constructor(arr?: Array<String|String[]>) {
         /** (string) 調用特殊繪製function */
         this.drawFunc = null
         /** (array) 資料內容 */
         this.content = arr || []
     }
-    get length() {
+    get length(): Number {
         return this.content.length
     }
     /**
@@ -16,12 +18,12 @@ export default class Section {
      * @param  {string} color
      * @return this
      */
-    add(text, color) {
-        const res = (color) ? [text, color] : text
+    add(text: String, color?: String): this {
+        const res: String|String[] = (color) ? [text, color] : text
         this.content.push(res)
         return this
     }
-    setDrawFunc(func) {
+    setDrawFunc(func: String) {
       this.drawFunc = func
       return this
     }
@@ -30,14 +32,14 @@ export default class Section {
      * @param  {array} array - array of 'text' or ['text', 'color']
      * @return this
      */
-    setContent(array) {
+    setContent(array: Array<String|String[]>) {
         this.content = array
         return this
     }
-    concat(array) {
+    concat(array: Array<String|String[]>) {
         this.content = this.content.concat(array)
     }
-    forEach(fn) {
+    forEach(fn: (value: String|String[], index: number, array: (String|String[])[]) => void) {
       this.content.forEach(fn)
     }
 }
